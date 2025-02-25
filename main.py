e8cf3a08edcfc2db0f4c818d1daf84a3aa1f914e
Jamie404 <jamie404notfound@gmail.com>
2025-02-25 12:56:20 +0000

    Init

---
+++ main.py
@@ -0,0 +1,19 @@
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+def check_variables():
+    print("Hi")
+    a = "Hello"
+    n = 1337
+    b, c = "word", "letter"
+    x, y, z = 0.135, "abc", False
+
+    n = n + 1
+    n += 1
+    #n++
+    z = None
+
+
+# Dunder main
+if __name__ == '__main__':
+    check_variables()
